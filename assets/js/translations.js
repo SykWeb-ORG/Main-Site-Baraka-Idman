e54f70d48f84fc504a38d2200b96a433923859bd
Mohamed YOUSFI <mr.mohamed.yousfi@gmail.com>
2023-06-04 10:52:45 +0100

    [BARAKA IDAMAN FRONT - 7][Traitement du tarduction]

---
+++ assets/js/translations.js
@@ -0,0 +1,43 @@
+const translations = {
+    en: {
+        home: "Home",
+        newsletter: "Newsletter",
+        news: "News",
+        activities: "Activities",
+        events: "Events",
+        contact: "Contact",
+        gallery: "Gallery",
+        hero_title: "Learn about activities and concerns in the Brookton neighborhood",
+        hero_desc: "The purpose of the Brookton Civic Association is to promote the civic welfare (to include, but not be limited to, social, economic, environmental, public safety, quality of life, and heritage issues/concerns).",
+        hero_first_btn: "Learn More",
+        hero_seconde_btn: "Get Involved"
+    },
+    fr: {
+        home: "Accueil",
+        newsletter: "Newsletter",
+        news: "Actulatié",
+        activities: "Activitées",
+        events: "Evenements",
+        contact: "Contact",
+        gallery: "Gallery",
+        hero_title: "En savoir plus sur les activités et les préoccupations du quartier de Brookton",
+        hero_desc: "Le but de la Brookton Civic Association est de promouvoir le bien-être civique (y compris, mais sans s'y limiter, les problèmes sociaux, économiques, environnementaux, de sécurité publique, de qualité de vie et de patrimoine).",
+        hero_first_btn: "Lire Plus",
+        hero_seconde_btn: "Être impliqué"
+    },
+    es: {
+        home: "Hogar",
+        newsletter: "Boletin informativo",
+        news: "Noticias",
+        activities: "Actividades",
+        events: "Eventos",
+        contact: "Contacto",
+        gallery: "Galería",
+        hero_title: "Infórmese sobre actividades e inquietudes en el vecindario de Brookton",
+        hero_desc: "The purpose of the Brookton Civic Association is to promote the civic welfare (to include, but not be limited to, social, economic, environmental, public safety, quality of life, and heritage issues/concerns).",
+        hero_first_btn: "Aprende más",
+        hero_seconde_btn: "Involucrarse"
+    }
+}
+
+export default translations
